19479bef54463b80cdfaf84a11c9bba0dbad13e1
Sambhu <sambhunathsahoo25@gmail.com>
2021-04-27 15:26:21 +0530

    added levelorder and line wise traversal

---
+++ Level 1/11_generic_tree/n06_linewise.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class n06_linewise {
+
+    public static class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+    public static Node construct(Integer arr[]) {
+
+        Stack<Node> st = new Stack<>();
+        Node root = new Node();
+        root.data = arr[0];
+        st.push(root);
+        for(int i=1; i<arr.length; i++) {
+            if(arr[i]==null){
+                st.pop();
+            }else{
+                Node nn = new Node();
+                nn.data = arr[i];
+                st.peek().children.add(nn);
+                st.push(nn);
+            }
+        }
+        return root;
+    }
+
+    public static void display(Node node) {
+        String str = node.data + " -> ";
+        for(int i=0; i<node.children.size(); i++){
+            str += node.children.get(i).data +", ";
+        }
+        str += ".";
+        System.out.println(str);
+        for(int i=0; i<node.children.size(); i++){
+            display(node.children.get(i));
+        }
+    }
+
+    public static void levelorderlinewise(Node node) {
+       Queue<Node> q = new ArrayDeque<>();
+       q.add(node);
+
+       while(q.size() > 0) {
+           int sz = q.size();
+           for(int i=0; i<sz; i++) {
+               Node rn = q.remove();
+               System.out.print(rn.data + " ");
+               for(Node child: rn.children) {
+                   q.add(child);
+               }
+           }
+           System.out.println();
+       }
+   }
+
+    public static void main(String args[]) {
+        Integer[] arr = {10, 20, 50, null, 60, null, null, 30, 70, null, 80, 110, null, 120, null, null, 90, null, null, 40, 100, null, null, null};
+        Node root = construct(arr);
+        levelorderlinewise(root);
+    }
+}

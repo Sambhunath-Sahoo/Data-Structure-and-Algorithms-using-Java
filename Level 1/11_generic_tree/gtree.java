0c477ecce3ef026738cef12a632f7448b9a5f02e
Sambhu <sambhunathsahoo25@gmail.com>
2021-04-27 14:56:03 +0530

    added construct ans display

--- Level 1/11_generic_tree/gtree.java
+++ Level 1/11_generic_tree/gtree.java
@@ -4,60 +4,51 @@ public class gtree {
 
     public static class Node {
         int data;
-        ArrayList<Node> children;
+        ArrayList<Node> children = new ArrayList<>();
+    }
 
-        public Node(int data) {
-            this.data = data;
-            this.children = new ArrayList<>();
+    public static Node construct(Integer arr[]) {
+
+        Stack<Node> st = new Stack<>();
+        Node root = new Node();
+        root.data = arr[0];
+        st.push(root);
+        for(int i=1; i<arr.length; i++) {
+            if(arr[i]==null){
+                st.pop();
+            }else{
+                Node nn = new Node();
+                nn.data = arr[i];
+                st.peek().children.add(nn);
+                st.push(nn);
+            }
         }
-    } 
+        return root;
+    }
 
     public static void display(Node node) {
-        // our work
         String str = node.data + " -> ";
-        for (Node child : node.children) {
-            str += child.data + ", ";
+        // for (Node child : node.children) {
+        //     str += child.data + ", ";
+        // }
+        for(int i=0; i<node.children.size(); i++){
+            str += node.children.get(i).data +", ";
         }
         str += ".";
         System.out.println(str);
         // faith
-        for (Node child : node.children) {
-            display(child);
+        // for (Node child : node.children) {
+        //     display(child);
+        // }
+        for(int i=0; i<node.children.size(); i++){
+            display(node.children.get(i));
         }
     }
 
-    public static Node construct(Integer[] data) {
-        Node root = null;
-
-        Stack<Node> st = new Stack<>();
-        for (int i = 0; i < data.length; i++) {
-            if (data[i] == null) {
-                st.pop();
-            } else {
-                //  make a new node
-                Node nn = new Node(data[i]);
-                // push into the children of stack top node
-                if (st.size() > 0) {
-                    st.peek().children.add(nn);
-                } else {
-                    root = nn;
-                }
-                // push into stack
-                st.push(nn);
-            }
-        }
-        return root; 
-    }
-
-
-    public static void fun() {
-        Integer[] data = {10, 20, 50, null, 60, null, null, 30, 70, null, 80, 110, null,
-             120, null, null, 90, null, null, 40, 100, null, null, null};
-        Node root = construct(data);
-        display(root);
-    }
 
     public static void main(String args[]) {
-        fun();
+        Integer[] arr = {10, 20, 50, null, 60, null, null, 30, 70, null, 80, 110, null, 120, null, null, 90, null, null, 40, 100, null, null, null};
+        Node root = construct(arr);
+        display(root);
     }
 }

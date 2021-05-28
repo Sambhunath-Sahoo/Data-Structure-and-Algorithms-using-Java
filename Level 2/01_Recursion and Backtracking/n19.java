8721289e3137f2f1e436f56d182c8e0087d30c37
Sambhu <sambhunathsahoo25@gmail.com>
2021-05-28 11:55:18 +0530

    added 19, 20

---
+++ Level 2/01_Recursion and Backtracking/n19.java
@@ -0,0 +1,46 @@
+// Words - K Length Words - 2
+// Words - K Length Words - 2
+import java.io.*;
+import java.util.*;
+
+public class n19 {
+
+    public static void generationofwords(int cs, int ts, String ustr, HashSet<Character> used, String asf) {
+
+        if(cs>ts){
+            System.out.println(asf);
+            return;
+        }
+
+        for(int i = 0; i < ustr.length(); i++) {
+            char ch = ustr.charAt(i);
+            if(used.contains(ch)== false) {
+                used.add(ch);
+                generationofwords(cs+1, ts, ustr, used, asf+ch);
+                used.remove(ch);
+            }
+        }
+    }
+ 
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    String str = br.readLine();
+    int k = Integer.parseInt(br.readLine());
+
+    HashSet<Character> unique = new HashSet<>();
+    String ustr = "";
+    
+    for (char ch : str.toCharArray()) {
+      if (unique.contains(ch) == false) {
+        unique.add(ch);
+        ustr += ch;
+      }
+    }
+
+   generationofwords(1, k, ustr, new HashSet<>(), "");
+
+  }
+
+}
